12e952d8f4246bb7c6e031bd8ed05c09d9cf0206
cyrilpointet <pointet.cyril@gmail.com>
2022-09-22 19:52:09 +0200

    Hide firebase key: use env var

--- config/larafirebase.php
+++ config/larafirebase.php
@@ -2,6 +2,6 @@
 
 return [
 
-    'authentication_key' => 'AAAAB9vVO9c:APA91bH1rLmR_bGRunRltsPtC3s3hF-Aj9DA1qPhENQ9iouKa3uLGTf3akf_eNPxoffMr1hIkEstfb2qygzuown4pwx2_PWkGwfLR8crgBB7BfC10-Iwbqhw-SnmVObzedLMWq8tCptW'
+    'authentication_key' => $_SERVER['FIREBASE_KEY']
 
 ];
